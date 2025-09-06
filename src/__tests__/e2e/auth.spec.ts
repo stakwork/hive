522c169742347694577e02e30c3d16bf22b97c89
Evanfeenstra <evanfeenstra@gmail.com>
2025-09-06 16:39:37 -0700

    try to fix e2e

--- src/__tests__/e2e/auth.spec.ts
+++ src/__tests__/e2e/auth.spec.ts
@@ -4,9 +4,7 @@ test("user can log in", async ({ page }) => {
   await page.goto("http://localhost:3000");
   await page.click('[data-testid="mock-signin-button"]');
   await page.waitForTimeout(400);
-  await expect(page).toHaveURL("http://localhost:3000/w/mock-stakgraph");
-  await expect(
-    page.locator("text=Welcome to your development workspace."),
-  ).toBeVisible();
+  await expect(page).toHaveURL("http://localhost:3000/w/mock-stakgraph-123");
+  await expect(page.locator("text=Welcome to your development workspace.")).toBeVisible();
   await page.waitForTimeout(400);
 });
