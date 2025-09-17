50af76f468b748bae1917517a75cbd821accee92
Evanfeenstra <evanfeenstra@gmail.com>
2025-09-17 11:10:55 -0700

    fix slug

--- src/app/w/[slug]/learn/page.tsx
+++ src/app/w/[slug]/learn/page.tsx
@@ -1,15 +1,17 @@
 import { LearnChat } from "./components/LearnChat";
 
 interface LearnPageProps {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
 }
 
-export default function LearnPage({ params }: LearnPageProps) {
+export default async function LearnPage({ params }: LearnPageProps) {
+  const { slug } = await params;
+
   return (
     <div className="flex-1 flex flex-col h-full">
-      <LearnChat workspaceSlug={params.slug} />
+      <LearnChat workspaceSlug={slug} />
     </div>
   );
 }
