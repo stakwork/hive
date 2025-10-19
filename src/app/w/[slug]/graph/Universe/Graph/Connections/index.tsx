e75b01cc783c1c0844b7add7475ff2b69b5a9a2e
Расул <rasul@MacBook-Pro-3.local>
2025-10-20 01:37:36 +0300

    feat: change type to string | NodeExtended

--- src/app/w/[slug]/graph/Universe/Graph/Connections/index.tsx
+++ src/app/w/[slug]/graph/Universe/Graph/Connections/index.tsx
@@ -2,7 +2,7 @@ import { useDataStore } from '@/stores/useDataStore'
 import { useGraphStore } from '@/stores/useGraphStore'
 import { useSimulationStore } from '@/stores/useSimulationStore'
 import { Segments } from '@react-three/drei'
-import { Link } from '@Universe/types'
+import { Link, NodeExtended } from '@Universe/types'
 import { memo } from 'react'
 import { LinkPosition } from '..'
 import { LineComponent } from './LineComponent'
@@ -38,7 +38,7 @@ export const Connections = memo(({ linksPosition }: Props) => {
       <group name="simulation-3d-group__connections">
         {dataInitial?.links?.length ? (
           <>
-            {dataInitial?.links.map((l: Link) => {
+            {dataInitial?.links.map((l: Link<string | NodeExtended>) => {
               const position = linksPosition.get(l.ref_id) || {
                 sx: 0,
                 sy: 0,
@@ -48,15 +48,18 @@ export const Connections = memo(({ linksPosition }: Props) => {
                 tz: 0,
               }
 
+              const sourceId = typeof l.source === 'string' ? l.source : l.source?.ref_id || ''
+              const targetId = typeof l.target === 'string' ? l.target : l.target?.ref_id || ''
+
               return (
                 <LineComponent
                   key={l.ref_id}
                   label={l.edge_type}
-                  source={l.source?.ref_id ? (l.source?.ref_id as string) : l.source || ''}
+                  source={sourceId}
                   sourceX={position.sx}
                   sourceY={position.sy}
                   sourceZ={position.sz}
-                  target={l.target?.ref_id ? (l.target?.ref_id as string) : l.target || ''}
+                  target={targetId}
                   targetX={position.tx}
                   targetY={position.ty}
                   targetZ={position.tz}
