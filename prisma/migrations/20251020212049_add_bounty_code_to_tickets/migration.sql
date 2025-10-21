a475401eb99e7010b0df3ca528512aba320c85d8
kelmith <faye.12222883@lpu.in>
2025-10-21 20:06:58 +0530

    fix: update migration to use tasks table instead of tickets

--- prisma/migrations/20251020212049_add_bounty_code_to_tickets/migration.sql
+++ prisma/migrations/20251020212049_add_bounty_code_to_tickets/migration.sql
@@ -1,14 +1,14 @@
 /*
   Warnings:
 
-  - A unique constraint covering the columns `[bounty_code]` on the table `tickets` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[bounty_code]` on the table `tasks` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- AlterTable
-ALTER TABLE "tickets" ADD COLUMN     "bounty_code" TEXT;
+ALTER TABLE "tasks" ADD COLUMN     "bounty_code" TEXT;
 
 -- CreateIndex
-CREATE UNIQUE INDEX "tickets_bounty_code_key" ON "tickets"("bounty_code");
+CREATE UNIQUE INDEX "tasks_bounty_code_key" ON "tasks"("bounty_code");
 
 -- CreateIndex
-CREATE INDEX "tickets_bounty_code_idx" ON "tickets"("bounty_code");
+CREATE INDEX "tasks_bounty_code_idx" ON "tasks"("bounty_code");
