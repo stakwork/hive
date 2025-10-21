fa2994b0dd99eabd5106284e4c403444fbf2a333
Расул <rasul@MacBook-Pro-3.local>
2025-10-21 19:09:02 +0300

    feat: move universe to components, update retrieving link type

---
+++ src/components/knowledge-graph/Universe/Graph/Cubes/NodePoints/Point/index.tsx
@@ -0,0 +1,31 @@
+import { NodeExtended } from '@/components/knowledge-graph/Universe/types'
+import { generatePalette } from '@/components/knowledge-graph/Universe/utils/palleteGenerator'
+import { Billboard, Instance } from '@react-three/drei'
+import { memo, useRef } from 'react'
+import { Group, Mesh } from 'three'
+import { nodeSize } from '../../constants'
+
+type Props = {
+  color: string
+  node: NodeExtended
+  scale: number
+}
+
+export const Point = memo(({ color, node, scale }: Props) => {
+  const nodeRef = useRef<Group | null>(null)
+  const helperRef = useRef<Mesh | null>(null)
+
+  const newColor = generatePalette(color, 3, 10)
+
+  return (
+    <Billboard ref={nodeRef} follow lockX={false} lockY={false} lockZ={false} name="group-name">
+      <mesh ref={helperRef} name="instance-helper" userData={node}>
+        <sphereGeometry args={[nodeSize / 2, 16, 16]} />
+        <meshBasicMaterial color={newColor.at(3)} opacity={1} transparent={false} />
+      </mesh>
+      <Instance color={newColor.at(3)} name="instance" scale={scale} />
+    </Billboard>
+  )
+})
+
+Point.displayName = 'Point'
