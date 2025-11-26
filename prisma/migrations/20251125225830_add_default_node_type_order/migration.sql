95a52d004f091541ceadb535f1b4f4a84bdce2ac
Rassl <rasul.sahibjanli@gmail.com>
2025-11-26 13:43:35 +0400

    feat: update migration file

--- prisma/migrations/20251125225830_add_default_node_type_order/migration.sql
+++ prisma/migrations/20251125225830_add_default_node_type_order/migration.sql
@@ -1,2 +1,4 @@
--- Add the node_type_order column with default value
-ALTER TABLE "workspaces" ADD COLUMN "node_type_order" JSONB DEFAULT '[{"type":"Function","value":20},{"type":"Feature","value":20},{"type":"File","value":20},{"type":"Endpoint","value":20},{"type":"Person","value":20},{"type":"Episode","value":20},{"type":"Call","value":20},{"type":"Message","value":20}]' NOT NULL;
+ALTER TABLE "workspaces"
+ADD COLUMN IF NOT EXISTS "node_type_order" JSONB
+DEFAULT '[{"type":"Function","value":20},{"type":"Feature","value":20},{"type":"File","value":20},{"type":"Endpoint","value":20},{"type":"Person","value":20},{"type":"Episode","value":20},{"type":"Call","value":20},{"type":"Message","value":20}]'
+NOT NULL;
