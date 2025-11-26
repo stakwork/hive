972c8236e6e7ca20d7fd6f12ca41be603f8dad8f
Rassl <rasul.sahibjanli@gmail.com>
2025-11-26 13:28:16 +0400

    feat: update migration file

--- prisma/migrations/20251125225830_add_default_node_type_order/migration.sql
+++ prisma/migrations/20251125225830_add_default_node_type_order/migration.sql
@@ -1,14 +1,2 @@
-/*
-  Warnings:
-
-  - Made the column `node_type_order` on table `workspaces` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- Update existing NULL values
-UPDATE "workspaces"
-SET "node_type_order" = '[{"type":"Function","value":20},{"type":"Feature","value":20},{"type":"File","value":20},{"type":"Endpoint","value":20},{"type":"Person","value":20},{"type":"Episode","value":20},{"type":"Call","value":20},{"type":"Message","value":20}]'
-WHERE "node_type_order" IS NULL;
-
--- AlterTable
-ALTER TABLE "workspaces" ALTER COLUMN "node_type_order" SET NOT NULL,
-ALTER COLUMN "node_type_order" SET DEFAULT '[{"type":"Function","value":20},{"type":"Feature","value":20},{"type":"File","value":20},{"type":"Endpoint","value":20},{"type":"Person","value":20},{"type":"Episode","value":20},{"type":"Call","value":20},{"type":"Message","value":20}]';
+-- Add the node_type_order column with default value
+ALTER TABLE "workspaces" ADD COLUMN "node_type_order" JSONB DEFAULT '[{"type":"Function","value":20},{"type":"Feature","value":20},{"type":"File","value":20},{"type":"Endpoint","value":20},{"type":"Person","value":20},{"type":"Episode","value":20},{"type":"Call","value":20},{"type":"Message","value":20}]' NOT NULL;
