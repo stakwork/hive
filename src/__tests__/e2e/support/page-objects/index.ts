60a5897def05197bca50f4c4f18a07e9424153f7
kevkevin <oapallikunnel@gmail.com>
2025-12-11 05:08:36 -0600

    Revert "implement protect janitors essential list visibility test"
    This reverts commit 0deee2b850cfabc4064d151eebb1fd4f341ff7aa.

--- src/__tests__/e2e/support/page-objects/index.ts
+++ src/__tests__/e2e/support/page-objects/index.ts
@@ -12,4 +12,3 @@ export { RoadmapPage } from './RoadmapPage';
 export { FeatureDetailPage } from './FeatureDetailPage';
 export { PhaseDetailPage } from './PhaseDetailPage';
 export { CallsPage } from './CallsPage';
-export { JanitorsPage } from './JanitorsPage';
