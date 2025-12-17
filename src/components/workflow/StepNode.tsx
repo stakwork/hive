4ccae93764df391592946fcc10aff92b6b6ebfe9
Gonzalo Javier Aune <gonzaloaune@gmail.com>
2025-12-17 16:54:27 +0000

    added publish feature to workflow editing

--- src/components/workflow/StepNode.tsx
+++ src/components/workflow/StepNode.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback } from 'react';
-import { Handle, Position, useHandleConnections, NodeProps } from '@xyflow/react';
+import React, { useCallback } from "react";
+import { Handle, Position, useHandleConnections, NodeProps } from "@xyflow/react";
 
 interface StepNodeData {
   id: string;
@@ -25,61 +25,66 @@ export default function StepNode({ data: rawData }: NodeProps) {
   }, []);
 
   const sourceConnections = useHandleConnections({
-    type: 'source',
+    type: "source",
   });
 
   const targetConnections = useHandleConnections({
-    type: 'target',
+    type: "target",
   });
 
-  let isSourceConnectable = data.id !== 'start';
-  let isTargetConnectable = data.id !== 'start';
+  let isSourceConnectable = data.id !== "start";
+  let isTargetConnectable = data.id !== "start";
 
-  if (data.id === 'system.succeed' || data.id === 'system.fail') {
+  if (data.id === "system.succeed" || data.id === "system.fail") {
     isTargetConnectable = true;
     isSourceConnectable = false;
   }
 
-  let dragHandleClass = data.project_view ? 'workflow-drag-handle__custom_small' : 'workflow-drag-handle__custom';
+  let dragHandleClass = data.project_view ? "workflow-drag-handle__custom_small" : "workflow-drag-handle__custom";
 
   return (
-    <div className={`nowheel ${data.project_view ? 'workflow-flow-project-view' : ''}`}>
-      { (data.id !== 'start') &&
+    <div className={`nowheel ${data.project_view ? "workflow-flow-project-view" : ""}`}>
+      {data.id !== "start" && (
         <Handle
           type="target"
           position={Position.Left}
-          className={data.stepType !== 'IfCondition' ? dragHandleClass : 'workflow-drag-handle__custom workflow-if-condition-left-handle'}
+          className={
+            data.stepType !== "IfCondition"
+              ? dragHandleClass
+              : "workflow-drag-handle__custom workflow-if-condition-left-handle"
+          }
           isConnectable={!data.project_view && isTargetConnectable}
         />
-      }
+      )}
 
-      <div className={`${data.className || ''}`} style={{
+      <div
+        className={`${data.className || ""}`}
+        style={{
           width: `${data.width || 100}px`,
           height: `${data.height || 50}px`,
-          pointerEvents: 'all',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          willChange: 'top, left',
-          backgroundColor: data.bgColor as React.CSSProperties['backgroundColor'],
+          pointerEvents: "all",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          willChange: "top, left",
+          backgroundColor: data.bgColor as React.CSSProperties["backgroundColor"],
           borderRadius: `${data.borderRadius}px`,
-          borderWidth: '2px',
-          borderStyle: 'solid',
-          borderColor: data.borderColor as React.CSSProperties['borderColor'],
-          color: data.textColor as React.CSSProperties['color']
-      }}>
-        <div style={{ width: '100%' }} dangerouslySetInnerHTML={{__html: data.data.html}}>
-
-        </div>
+          borderWidth: "2px",
+          borderStyle: "solid",
+          borderColor: data.borderColor as React.CSSProperties["borderColor"],
+          color: data.textColor as React.CSSProperties["color"],
+        }}
+      >
+        <div style={{ width: "100%" }} dangerouslySetInnerHTML={{ __html: data.data.html }}></div>
       </div>
-      { (data.id !== 'system.fail' && data.id !== 'system.succeed') &&
+      {data.id !== "system.fail" && data.id !== "system.succeed" && (
         <Handle
           type="source"
           position={Position.Right}
-          className={data.stepType !== 'IfCondition' ? dragHandleClass : 'workflow-if-condition-handle'}
-          isConnectable={data.stepType !== 'IfCondition' ? (!data.project_view && isSourceConnectable) : false}
+          className={data.stepType !== "IfCondition" ? dragHandleClass : "workflow-if-condition-handle"}
+          isConnectable={data.stepType !== "IfCondition" ? !data.project_view && isSourceConnectable : false}
         />
-      }
+      )}
     </div>
   );
 }
