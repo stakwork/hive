049cfd66c96fafb172c249a2ee929668d1d22e4b
pitoi <pitoi@users.noreply.github.com>
2026-01-18 18:29:32 +0000

    add view task action for non-todo tasks in roadmaptaskstable

--- src/components/ui/action-menu.tsx
+++ src/components/ui/action-menu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { MoreVertical, LucideIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
