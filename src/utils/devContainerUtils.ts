db71cf614de4692ada743e70c3047808ae2f5713
Evanfeenstra <evanfeenstra@gmail.com>
2026-06-04 11:48:01 -0700

    empty default docker compose

--- src/utils/devContainerUtils.ts
+++ src/utils/devContainerUtils.ts
@@ -330,23 +330,7 @@ export function devcontainerJsonContent(repoName: string) {
 }
 
 export function dockerComposeContent() {
-  return `version: '3.8'
-networks:
-  app_network:
-    driver: bridge
-services:
-  app:
-    build:
-      context: .
-      dockerfile: Dockerfile
-    volumes:
-      - ../..:/workspaces:cached
-    command: sleep infinity
-    networks:
-      - app_network
-    extra_hosts:
-      - "localhost:172.17.0.1"
-      - "host.docker.internal:host-gateway"
+  return `services: {}
 `;
 }
 
